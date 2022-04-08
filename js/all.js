f1889e0a25831987611da42ab7b20d0f64e111be
d93fu6513 <102431864+d93fu6513@users.noreply.github.com>
2022-04-09 02:32:15 +0800

    add

---
+++ js/all.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // 捲軸偵測距離頂部超過 50 才顯示按鈕
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 50) {
+        if ($(".back-top").hasClass("hide")) {
+          $(".back-top").toggleClass("hide");
+        }
+      } else {
+        $(".back-top").addClass("hide");
+      }
+    });
+});
